6817ebb2590e26f87d2cb75d171dfd9c44a2b7fa
Ignacio <ifacello@mi.unc.edu.ar>
2026-04-16 12:03:21 +0000

    feat: configuration script

---
+++ setup.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# ==============================================================================
+# Repo installation script
+# ==============================================================================
+
+set -Eeuo pipefail
+
+echo "Running branch-specific setup for: $REPO_BRANCH"
+
+eval "$("${CONDA_PATH}/bin/conda" 'shell.bash' 'hook')"
+
+conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/main
+conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/r
+
+if ! conda info --envs | grep -q "unifolm-wma"; then
+    conda create --yes -n ${REPO_BRANCH} python=3.10.19
+fi
+
+conda activate ${REPO_BRANCH}
+
+conda install pinocchio=3.2.0 -c conda-forge -y
+conda install ffmpeg=7.1.1 -c conda-forge
+
+pip install torch torchvision --index-url https://download.pytorch.org/whl/rocm7.0
+
+pip install psutil ninja
+pip install flash-attn --no-build-isolation
+
+cd ${MAIN_DIR}
+git submodule update --init --recursive
+
+pip install -e .
+
+cd external/dlimp
+pip install -e .
+
+cd ../../..
+
+echo "Branch-specific setup complete."
